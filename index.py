10987691c061e209a2e1cbd9ab9bde5d2c37e802
yuriykuntsyak <yuriykuntsyak@gmail.com>
2021-02-19 22:47:52 +0000

    test 3

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World test 2',
+        'output': 'Hello World test 3',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
