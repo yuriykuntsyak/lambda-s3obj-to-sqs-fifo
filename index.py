9f612d50a5f5b7198960452eb5218652b5806dbc
yuriykuntsyak <yuriykuntsyak@gmail.com>
2021-02-19 23:29:51 +0000

    added lambda_s3_handler

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import json
 import datetime
+import urllib.parse
+import boto3
 
+s3 = boto3.client('s3')
 
 def handler(event, context):
     data = {
@@ -10,3 +13,19 @@ def handler(event, context):
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
+
+def lambda_s3_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
